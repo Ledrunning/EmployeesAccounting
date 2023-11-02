0582e30b1aad8aede9e4de91b58ff9fd6649d70e
Ledrunning <ledrunner@hotmail.com>
2023-11-02 21:45:24 +0100

    Hid password in textbox

---
+++ EA.DesktopApp/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EA.DesktopApp.Helpers
+{
+    public class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordBoxHelper),
+                new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static string GetBoundPassword(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject obj, string value)
+        {
+            obj.SetValue(BoundPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(DependencyObject dObj, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(dObj is PasswordBox passwordBox))
+            {
+                return;
+            }
+
+            passwordBox.PasswordChanged -= PasswordChanged;
+            passwordBox.Password = e.NewValue != null ? e.NewValue.ToString() : string.Empty;
+
+            passwordBox.PasswordChanged += PasswordChanged;
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                SetBoundPassword(passwordBox, passwordBox.Password);
+            }
+        }
+    }
+}
